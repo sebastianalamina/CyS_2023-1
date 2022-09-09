7a2a19fe7f91db33bf255cde1dcda4600a3321eb
Sebastián <alamina@ciencias.unam.mx>
2022-09-08 19:15:00 -0500

    P01 | Clase Aux (para el manejo de cadenas y matrices) añadida.

---
+++ P01/src/aux/Aux.java
@@ -0,0 +1,42 @@
+package aux;
+
+/**
+ * Clase Aux.
+ * Auxilia en el manejo de cadenas y arreglos.
+ * Simula una clase estática, por lo que todos
+ * sus métodos han de ser estáticos (similar al
+ * funcionamiento de la clase Math).
+ */
+public final class Aux extends Object {
+
+	/**
+	 * Constructor de la clase Aux.
+	 * Es privado para evitar instanciación, y no hace nada.
+	 */
+	private Aux() {}
+
+	/**
+	 * Toma una cadena y la normaliza. Es decir, le quita signos
+	 * de puntuación, acentos, espacios en blanco, y convierte
+	 * las minúsculas en mayúsculas.
+	 * @param texto Texto que será normalizado.
+	 * @return Texto ya normalizado.
+	 */
+	public static String normalizarTexto(String texto) {
+
+		return "";
+	}
+
+	/**
+	 * Toma dos matrices (de enteros) y las multiplica.
+	 * @param a1 Arreglo izquierdo a multiplicar.
+	 * @param a2 Arreglo derecho a multiplicar.
+	 * @return La matriz que resulta de multiplicar los
+	 * arreglos de los parámetros.
+	 */
+	public static int[] multiplicarArreglos(int[] a1, int[] a2) {
+
+		return new int[0];
+	}
+
+}
